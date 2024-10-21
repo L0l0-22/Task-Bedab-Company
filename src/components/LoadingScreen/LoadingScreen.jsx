a64c7a89ea56d1f886ca50785f39d9d19d479610
L0l0-22 <lilianaziz2001@gmail.com>
2024-10-22 01:25:31 +0300

    Task Bedab Company

---
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+
+const LoadingScreen = ({ onImagesLoaded }) => {
+    useEffect(() => {
+        const Loading = () => {
+            setTimeout(() => { onImagesLoaded(); 
+            }, 2000); 
+        };
+        Loading(); 
+    }, [onImagesLoaded]);
+
+    return (
+        <div className='min-h-96 flex items-center justify-center my-24'>
+            <i className='fas fa-spinner fa-spin fa-5x'></i>
+        </div>
+    );
+};
+
+export default LoadingScreen;
